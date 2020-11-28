bb6356411fbc55dbba41e4f82b0363f1f0a527f1
michał chałupczak <chomik@users.noreply.github.com>
2020-11-28 19:14:41 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,5 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(input.temperature())
 })
 basic.forever(function () {
-    while (true) {
-        serial.writeNumbers([input.temperature(), input.lightLevel()])
-        basic.pause(5000)
-    }
+	
 })
